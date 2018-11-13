1707d0eb8bb533919ea7b5715c7f9951ecd7cd25
Stiven Deleur <deleur@usc.edu>
2018-11-12 21:48:59 -0800

    better

--- scraper.js
+++ scraper.js
@@ -5,51 +5,55 @@ const ObjectsToCsv = require('objects-to-csv');
 const fs = require('fs');
 const program = require('commander');
 
+program.version('0.1.0').parse(process.argv);
 
-program.version('0.1.0')
-	.parse(process.argv);
+const inputFiles = program.args.slice(0, program.args.length - 1);
 
-
-const inputFile = program.args[0]
-
-const outputFile = program.args[1]
-
-
-
-const file = fs.readFileSync(__dirname + "/" + inputFile).toString();
-
-const $ = cheerio.load(file);
+const outputFile = program.args[program.args.length - 1];
 
 let Persons = [];
-
+let $ = null;
 
 function ScrapePerson(row) {
-	let firstName = $(`#search-results-data-table-row-${row}-cell-0`).first().text();
-	let lastName = $(`#search-results-data-table-row-${row}-cell-1`).first().text();
-	let company = $(`#search-results-data-table-row-${row}-cell-2`).first().text();
-	let email = $(`#search-results-data-table-row-${row}-cell-3`).first().text();
+  let firstName = $(`#search-results-data-table-row-${row}-cell-0`)
+    .first()
+    .text();
+  let lastName = $(`#search-results-data-table-row-${row}-cell-1`)
+    .first()
+    .text();
+  let company = $(`#search-results-data-table-row-${row}-cell-2`)
+    .first()
+    .text();
+  let email = $(`#search-results-data-table-row-${row}-cell-3`)
+    .first()
+    .text();
+  let position = $(`#search-results-data-table-row-${row}-cell-4`)
+    .first()
+    .text();
 
-	let person = {
-		firstName,
-		lastName,
-		company,
-		email
-	}
-	if (firstName == "" && lastName == "" && company == "") {
-		return null
-	}
-	return person;
+  let person = {
+    firstName,
+    lastName,
+    company,
+    email,
+    position,
+  };
+  if (firstName == '' && lastName == '' && company == '') {
+    return null;
+  }
+  return person;
 }
 
-
-for (let i = 0; i < 300; i++) {
-	let person = ScrapePerson(i);
-	if (person == null) break;
-	Persons.push(person);
-
-
-}
-Persons = Persons.filter(person => !!person.email.trim())
+inputFiles.forEach(inputFile => {
+  const file = fs.readFileSync(__dirname + '/' + inputFile).toString();
+  $ = cheerio.load(file);
+  for (let i = 0; i < 300; i++) {
+    let person = ScrapePerson(i);
+    if (person == null) break;
+    Persons.push(person);
+  }
+  Persons = Persons.filter(person => !!person.email.trim());
+});
 
 let csv = new ObjectsToCsv(Persons);
 
